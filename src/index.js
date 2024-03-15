32ff9d81965cc5d19cd93eadc5d7f4b5e689e517
milkbottle0305 <bigtog0602@gmail.com>
2024-03-15 12:29:25 +0900

    Fix: Broswer Route에서 Hash Route로 변경

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
   <RecoilRoot>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </RecoilRoot>,
   //</React.StrictMode>,
 );
